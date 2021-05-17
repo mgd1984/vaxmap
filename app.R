01ef94beb08108be969d684c52810ce21ecc116c
mgd1984 <alecjohnmiller@gmail.com>
2021-05-17 19:39:41 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -1,102 +1,206 @@
-
-library(httr)
-library(jsonlite)
 library(leaflet)
 library(leaflet.extras)
-library(htmltools)
-library(mapboxapi)
 library(shiny)
-library(shinyWidgets)
 library(shinydashboard)
+library(shinyWidgets)
+library(ggmap)
+library(geosphere)
+library(mapboxapi)
+library(htmltools)
+library(dplyr)
+library(RColorBrewer)
+library(viridis)
 
-ClinicCoordinates.df.final <- readRDS("ClinicAddresses.rds")
+source("API_KEY.r")
 
-data <- ClinicCoordinates.df.final
+register_google(API_KEY)
 
-# Define Shiny App UI
-header1 <- dashboardHeader(title = "VaxMap")
+data <- readRDS("ClinicAddresses.rds") #loads data
+colnames(data)[2:3] <- c("lng", "lat")
+data <- data[, c(1, 3, 2, 4:8)]
 
-sidebar1 <-
-    dashboardSidebar(sidebarMenu(
-        menuItem(
-            searchInput(
-                inputId = "city",
-                label = "City",
-                value = "Toronto",
-                placeholder = NULL
-            )
+
+map_header <- dashboardHeader(title = "VaxMap",
+                              titleWidth = 275)
+
+map_sidebar <- dashboardSidebar(
+  width = 275,
+  sidebarMenu(
+    id = 'sidebar1',
+    menuItem(
+      text = "Search Locations",
+      textInput(
+        inputId = "userlocation",
+        label = "",
+        placeholder = "Search Postal"
+      ),
+      actionButton("go", "Search"),
+      numericInput(
+        inputId = "distance",
+        label = "Radius (km)",
+        min = 0,
+        max = 500,
+        value = 5,
+        width = 120,
+        step = .5
+      )
+    )
+  ),
+  sidebarMenu(
+    id = "sidebar2",
+    width = "250px",
+    menuItem(
+      text = "Search Appointments Dates",
+      dateInput(
+        inputId = "date",
+        label = "Earliest Availability",
+        width = "250px"
+      ),
+      selectInput(
+        inputId = "age",
+        label = "Age",
+        choices = c("18+", "30+", "40+", "50+", "55+"),
+        width = "250px"
+      ),
+      selectInput(
+        inputId = "requirements",
+        label = "Risk Factor",
+        choices = c(
+          "High-Risk",
+          "Medical Conditions",
+          "Healthcare Workers",
+          "HotSpot",
+          "Indigenous Adults",
+          "Caregivers"
         )
-    ))
+      )
+    )
+  )
+)
 
-body1 <- dashboardBody(fluidPage(
-    
+map_body <- dashboardBody(
+  fluidPage(
     title = "Map",
     width = "100%",
     solidHeader = TRUE,
     status = "primary",
-    leafletOutput("baseMap"),
-    tags$style(type = "text/css", "#baseMap {height: calc(100vh - 80px) !important;}"),
-    tags$head(tags$script(src = "http://leaflet.github.io/Leaflet.heat/dist/leaflet-heat.js")),
-    tags$head(tags$style("#carrier {color: red; font-size: 20px;font-style: italic;}")),
-)
+    leafletOutput("mymap"),
+    tags$style(type = "text/css", "#mymap {height: calc(100vh - 80px) !important;}")
+  )
 )
 
-ui <- dashboardPage(header1, sidebar1, body1)
+ui <- dashboardPage(map_header, map_sidebar, map_body)
 
-# Define SERVER logic
-server <- function(input, output, session) {
-    
-    filteredData <- reactive({
-        data[data$city %in% input$city[1],]
+server3 <- function(input, output, session) {
+  distance_clinic_reactive <- eventReactive(input$go, {
+    address_latlon <- geocode(input$userlocation)
+    dist <- distm(
+      x = matrix(data = c(data$lng, data$lat), ncol = 2),
+      y = c(lon = address_latlon$lon, lat = address_latlon$lat),
+      fun = distVincentySphere
+    )
+    dist <- dist / 1000
+  })
+  
+  clinic_reactive <-
+    reactive({
+      data[distance_clinic_reactive() < input$distance,]
     })
+  
+  output$mymap <- renderLeaflet({
+    map <- leaflet() %>%
+      addTiles() %>%
+      addProviderTiles(providers$CartoDB.Voyager,
+                       group = "Voyager",
+                       tileOptions(useCache = TRUE, crossOrigin = T)) %>%
+      addProviderTiles(
+        providers$Esri.WorldImagery,
+        group = "Satellite",
+        tileOptions(useCache = TRUE, crossOrigin = T)
+      ) %>%
+      addProviderTiles(providers$CartoDB.Positron,
+                       group = "CartoDB",
+                       tileOptions(useCache = TRUE, crossOrigin = T)) %>%
+      addProviderTiles(providers$OpenStreetMap,
+                       group = "OSM",
+                       tileOptions(useCache = TRUE, crossOrigin = T)) %>%
+      addFullscreenControl(pseudoFullscreen = F) %>%
+      addLayersControl(
+        baseGroups = c("CartoDB", "OSM", "Satellite", "Voyager"),
+        overlayGroups = c("Pins", "Heatmap"),
+        options = layersControlOptions(collapsed = FALSE)) %>% 
+      hideGroup("Heatmap") %>% 
+        setView(
+        lng = mean(data$lng),
+        lat = mean(data$lat),
+        zoom = 9
+      )
+    map
+  })
+  
+  observe({
+    icon.fa <- makeAwesomeIcon(
+      icon = "hospital",
+      markerColor = "blue",
+      library = "fa",
+      iconColor = "white"
+    )
     
-    output$baseMap <- renderLeaflet({
-        leaflet() %>%
-            setView(-80,44,zoom=8) %>% 
-            enableTileCaching() %>% 
-            addProviderTiles(providers$CartoDB.DarkMatter, group = "Dark",tileOptions(useCache=TRUE, crossOrigin=T)) %>%
-            addProviderTiles(providers$Esri.WorldImagery, group = "Satellite",tileOptions(useCache=TRUE, crossOrigin=T)) %>%
-            addProviderTiles(providers$CartoDB.Positron, group = "CartoDB",tileOptions(useCache=TRUE, crossOrigin=T)) %>%
-            addProviderTiles(providers$OpenStreetMap, group = "OSM",tileOptions(useCache=TRUE, crossOrigin=T)) %>%
-            addFullscreenControl(pseudoFullscreen = F) %>%
-            addLayersControl(baseGroups = c("CartoDB", "OSM", "Satellite", "Dark"))
-    })
+    # pal <- color(
+    #   palette = "viridis",
+    #   domain = data$city)
     
-    observe({
+    content <-
+      paste0(
+        "<a href='",
+        "https://maps.google.com/maps/search/",
+        clinic_reactive()$full_address,
+        "' target='_blank'>",
+        "Google Maps Link</a>",
+        "<br>",
+        clinic_reactive()$line1
+      )
+    
+    leafletProxy("mymap") %>%
+      clearMarkers() %>%
+      clearHeatmap() %>%
+      # addLayersControl(c("Heatmap","Pins")) %>% 
+      addHeatmap(
+        data = clinic_reactive(),
+        lng = ~ lng,
+        lat = ~ lat,
+        # gradient = pal(data$city),
+        # intensity = ~sqrt(input$distance),
+        blur = 35,
+        gradient = "magma",
+        max = 1,
+        radius = 15,
+        cellSize = 5,
+        group = "Heatmap") %>% 
+      addAwesomeMarkers(
+        data = clinic_reactive(),
+        lng = ~ lng,
+        lat = ~ lat,
+        icon = icon.fa,
+        group = "Pins",
+        popup = content
+        # popup = paste0("<a href='","https://maps.google.com/maps/search/","vaccine+location/","@",
+        # clinic_reactive()$lat,",",clinic_reactive()$lng,",","16z","' target='_blank'>","Google Maps Link</a>")
+        # http://maps.google.com/maps/place/<name>/@<lat>,<long>,15z/data=<mode-value>
+        # https://maps.google.com?q=@51.03841,-114.01679
+        # popup = htmlEscape(apt_reactive()[,"full_address"]
         
-        icon.fa <- makeAwesomeIcon(icon = "hospital", 
-                                   markerColor = "pink", 
-                                   library = "fa",
-                                   iconColor = "white")
-        # color_scheme <- viridis::cividis(n_distinct(data$LICENSEE %in% c("Bell Mobility Inc.", "Bell Canada")))
-        # pal = colorFactor(color_scheme, data$LICENSEE)
-        leafletProxy("baseMap",data = filteredData()) %>%
-            clearMarkers() %>% 
-            clearMarkerClusters() %>% 
-            addAwesomeMarkers(
-                lng = ~X1,
-                lat = ~X2,
-                icon = icon.fa,
-                clusterOptions = markerClusterOptions(),
-                # minOpacity = .25, 
-                # max = 1,
-                # radius = 3, 
-                # blur = 15, 
-                # gradient = viridis::magma(5), 
-                # cellSize = 2) %>% 
-                popup = htmlEscape(filteredData()[,"full_address"]))%>% 
-            fitBounds(
-                ~ min(as.numeric(X1)),
-                ~ min(as.numeric(X2)),
-                ~ max(as.numeric(X1)),
-                ~ max(as.numeric(X2))
-            )
-        
-    })
-    
-} #server
+      ) %>%
+      fitBounds(
+        lng1 = min(clinic_reactive()$lng),
+        lat1 = min(clinic_reactive()$lat),
+        lng2 = max(clinic_reactive()$lng),
+        lat2 = max(clinic_reactive()$lat)
+      )
+  })
+}
+
+shinyApp(ui, server3)
 
 
-# Run app
-shinyApp(ui, server)
-
+## h/t to @iatowks via https://bit.ly/3fnxCon 
